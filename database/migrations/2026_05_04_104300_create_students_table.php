93272ec4a84bc06669bf06249acac0cd368a7a64
aizavilladolid <aizavilladolid8@gmail.com>
2026-05-04 18:57:43 +0800

    Added students table migration

---
+++ database/migrations/2026_05_04_104300_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void{
+    Schema::create('students', function (Blueprint $table) {
+        $table->id();
+        $table->string('student_id')->unique();   // school ID
+        $table->string('full_name');              // student name
+        $table->string('grade_level');            // grade/year level
+        $table->string('section')->nullable();    // optional section
+        $table->string('guardian_name')->nullable(); // parent/guardian
+        $table->string('contact_info')->nullable();  // phone/email
+        $table->timestamps();                     // created_at & updated_at
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
