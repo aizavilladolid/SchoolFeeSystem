913a580077aac0a7231788acb0ec95f6b545a219
aizavilladolid <aizavilladolid8@gmail.com>
2026-05-04 19:09:51 +0800

    Added payments table migration

---
+++ database/migrations/2026_05_04_110538_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('payments', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('student_id');   // link to students
+        $table->unsignedBigInteger('fee_id');       // link to fees
+        $table->decimal('amount_paid', 10, 2);      // payment amount
+        $table->date('payment_date');               // when paid
+        $table->string('payment_method');           // cash/check/online
+        $table->timestamps();
+
+        // Foreign keys
+        $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+        $table->foreign('fee_id')->references('id')->on('fees')->onDelete('cascade');
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
