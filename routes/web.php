53441752e086e025dee8fdddef6c96846b18359b
aizavilladolid <aizavilladolid8@gmail.com>
2026-05-11 23:38:12 +0800

    ga add kog balance  route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BalanceController;
 
+
+Route::get('/balance', [BalanceController::class, 'index'])->name('balance.index');
 Route::get('/', function () {
     return view('welcome');
 });
